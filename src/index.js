e6f0b93bc3652b507aa41df017e643b87bf41067
Zair Bulos <buloszair@gmail.com>
2025-04-19 12:44:12 -0300

    feat: Setup Discord bot client

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 require("module-alias/register");
+
+const { BOT_TOKEN } = require("@/config/config");
+const client = require("@/bot/client");
 const logger = require("@/config/logger");
 
-logger.info("Hello, world!");
+client
+  .login(BOT_TOKEN)
+  .then(() => {
+    logger.info("Bot is online!");
+  })
+  .catch((err) => {
+    logger.error("Failed to login:", err);
+  });
