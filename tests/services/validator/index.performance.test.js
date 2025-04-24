486f3c581a2ee45b66607eb723797c839eb55e5c
Zair Bulos <buloszair@gmail.com>
2025-04-24 18:52:30 -0300

    test: Add performance test for SpamValidator

---
+++ tests/services/validator/index.performance.test.js
@@ -0,0 +1,25 @@
+const spamValidator = require("@/services/validator");
+const { performance } = require("perf_hooks");
+
+describe("SpamValidator Performance", () => {
+  test("should handle thousands of messages efficiently", () => {
+    const spamMessage = "$50 gift https://www.malicious-link.com";
+    const cleanMessage =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mauris velit, molestie non accumsan et, pretium id libero.";
+
+    const spamMessages = Array(100000).fill(spamMessage);
+    const cleanMessages = Array(100000).fill(cleanMessage);
+    const messages = [...spamMessages, ...cleanMessages];
+
+    const startTime = performance.now();
+    messages.forEach((message) => {
+      spamValidator.isSpam(message);
+    });
+    const endTime = performance.now();
+
+    const duration = endTime - startTime;
+    console.log(`Validated ${messages.length} messages in ${duration}ms`);
+
+    expect(duration).toBeLessThan(30);
+  });
+});
