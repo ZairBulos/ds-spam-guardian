f818ec97f2844b47acf3215513d1c1c1b8804d67
Zair Bulos <buloszair@gmail.com>
2025-04-25 11:42:05 -0300

    feat: Add bot presence configuration

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -1,10 +1,12 @@
 const { Events } = require("discord.js");
 const logger = require("@/config/logger");
+const bot = require("@/config/bot");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
+    client.user.setPresence(bot);
     logger.info(`Ready! Logged in as ${client.user.tag}`);
   },
 };
---
+++ src/config/bot.js
@@ -0,0 +1,11 @@
+const { ActivityType } = require("discord.js");
+
+module.exports = {
+  activities: [
+    {
+      name: "Monitoreo de spam",
+      type: ActivityType.Watching,
+    },
+  ],
+  status: "online",
+};
