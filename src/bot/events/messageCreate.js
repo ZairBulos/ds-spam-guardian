01e2c319490a360208fd6044d350f462286b3da9
Zair Bulos <buloszair@gmail.com>
2025-04-25 11:32:49 -0300

    feat: Integrate spam validation in messageCreate event

--- src/bot/events/messageCreate.js
+++ src/bot/events/messageCreate.js
@@ -1,6 +1,7 @@
 const { Events } = require("discord.js");
 const logger = require("@/config/logger.js");
 const preprocessMessage = require("@/middlewares/preprocessMessage");
+const spamValidator = require("@/services/validator");
 
 module.exports = {
   name: Events.MessageCreate,
@@ -9,7 +10,13 @@ module.exports = {
 
     try {
       const cleanMessage = preprocessMessage(message.content);
-      console.log(`Message from ${message.author.tag}: ${cleanMessage}`);
+
+      if (spamValidator.isSpam(cleanMessage)) {
+        await message.delete();
+        logger.info(
+          `Spam detected from ${message.author.tag}: ${message.content}`,
+        );
+      }
     } catch (err) {
       logger.error(
         `Error processing message from ${message.author.tag}: ${err}`,
