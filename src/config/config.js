cf8f145400d35054127efd437f8a8d8935fc9dfa
Zair Bulos <buloszair@gmail.com>
2025-04-26 13:02:57 -0300

    build: Add Docker configuration for development environment

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,8 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+const path = require("path");
+
+const NODE_ENV = process.env.NODE_ENV || "development";
+dotenv.config({ path: path.join(__dirname, `../../.env.${NODE_ENV}`) });
 
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
