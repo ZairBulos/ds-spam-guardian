de3ecdc09a03ff316a1125c7f0ae82274f47170b
Zair Bulos <buloszair@gmail.com>
2025-05-03 11:42:13 -0300

    test: Restrict performance test to local environment only

--- tests/services/validator/index.performance.test.js
+++ tests/services/validator/index.performance.test.js
@@ -2,25 +2,26 @@ const spamValidator = require("@/services/validator");
 const { performance } = require("perf_hooks");
 
 describe("SpamValidator Performance", () => {
-  test("should handle thousands of messages efficiently", () => {
-    const spamMessage = "$50 gift https://www.malicious-link.com";
-    const cleanMessage =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mauris velit, molestie non accumsan et, pretium id libero.";
-
-    const spamMessages = Array(100000).fill(spamMessage);
-    const cleanMessages = Array(100000).fill(cleanMessage);
-    const messages = [...spamMessages, ...cleanMessages];
-
-    const startTime = performance.now();
-    messages.forEach((message) => {
-      spamValidator.isSpam(message);
+  if (!process.env.CI) {
+    test("should handle thousands of messages efficiently", () => {
+      const spamMessage = "$50 gift https://www.malicious-link.com";
+      const cleanMessage =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mauris velit, molestie non accumsan et, pretium id libero.";
+  
+      const spamMessages = Array(100000).fill(spamMessage);
+      const cleanMessages = Array(100000).fill(cleanMessage);
+      const messages = [...spamMessages, ...cleanMessages];
+  
+      const startTime = performance.now();
+      messages.forEach((message) => {
+        spamValidator.isSpam(message);
+      });
+      const endTime = performance.now();
+  
+      const duration = endTime - startTime;
+      console.log(`Validated ${messages.length} messages in ${duration}ms`);
+  
+      expect(duration).toBeLessThan(30);
     });
-    const endTime = performance.now();
-
-    const duration = endTime - startTime;
-    console.log(`Validated ${messages.length} messages in ${duration}ms`);
-
-    const performanceThreshold = process.env.CI ? 60 : 30;
-    expect(duration).toBeLessThan(performanceThreshold);
-  });
+  }
 });
