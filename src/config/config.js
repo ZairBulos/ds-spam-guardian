d72b67984755b37971e8e8c736a4c129d00a6cde
Zair Bulos <buloszair@gmail.com>
2025-06-02 18:26:49 -0300

    chore: Remove dotenv usage and switch to native --env-file for environment variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,5 @@
-const dotenv = require("dotenv");
-const path = require("path");
-
-const NODE_ENV = process.env.NODE_ENV || "development";
-dotenv.config({ path: path.join(__dirname, `../../.env.${NODE_ENV}`) });
-
 module.exports = {
-  NODE_ENV,
+  NODE_ENV: process.env.NODE_ENV,
   BOT_TOKEN: process.env.BOT_TOKEN,
   CLIENT_ID: process.env.CLIENT_ID,
   GUILD_ID: process.env.GUILD_ID,
