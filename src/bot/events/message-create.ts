3934d18c12a6cbae422680bb482810a43c253672
Zair Bulos <buloszair@gmail.com>
2025-06-24 18:18:32 -0300

    feat: integrate spam cleanup lock in message-create event handler

--- src/bot/events/message-create.ts
+++ src/bot/events/message-create.ts
@@ -2,15 +2,21 @@ import { Events, Message } from "discord.js";
 import { Event } from "../../core/interfaces/event";
 import { spamDetector } from "../../features/spam/providers/spam-detector-memory.provider";
 import { SpamCleaner } from "../../features/spam/services/spam-cleaner.service";
+import { SpamCleanupLock } from "../../features/spam/services/spam-cleanup-lock.service";
 
 const spamCleaner = new SpamCleaner();
+const spamCleanupLock = new SpamCleanupLock();
 
 class MessageCreateEvent implements Event<Events.MessageCreate> {
   public name = Events.MessageCreate;
 
   public async execute(message: Message): Promise<void> {
     const spamDetected = await spamDetector.isSpam(message);
-    if (spamDetected) void spamCleaner.deleteSpamMessages(message);
+    if (!spamDetected) return;
+
+    void spamCleanupLock.run(message.author.id, async () => {
+      await spamCleaner.deleteSpamMessages(message);
+    });
   }
 }
 
