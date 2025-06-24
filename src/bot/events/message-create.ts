26b4f1095301ef1ab7d191d688d2949b3fe4f669
Zair Bulos <buloszair@gmail.com>
2025-06-24 18:00:48 -0300

    feat: integrate spam cleaner in message-create event handler

--- src/bot/events/message-create.ts
+++ src/bot/events/message-create.ts
@@ -1,15 +1,16 @@
 import { Events, Message } from "discord.js";
 import { Event } from "../../core/interfaces/event";
-import { spamDetector } from "../../features/spam/providers/spam-detector-redis.provider";
+import { spamDetector } from "../../features/spam/providers/spam-detector-memory.provider";
+import { SpamCleaner } from "../../features/spam/services/spam-cleaner.service";
+
+const spamCleaner = new SpamCleaner();
 
 class MessageCreateEvent implements Event<Events.MessageCreate> {
   public name = Events.MessageCreate;
 
   public async execute(message: Message): Promise<void> {
     const spamDetected = await spamDetector.isSpam(message);
-    if (spamDetected) {
-      console.warn(`[SPAM DETECTED] ${message.author.tag}: ${message.content}`);
-    }
+    if (spamDetected) void spamCleaner.deleteSpamMessages(message);
   }
 }
 
