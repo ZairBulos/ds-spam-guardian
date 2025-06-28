fdb7f0d56cf05c2aef45cc47d3d1f32e01225a92
Zair Bulos <buloszair@gmail.com>
2025-06-28 18:33:40 -0300

    chore: remove unused KeyValueStore interface

--- src/core/contracts/storage/key-value-store.ts
+++
@@ -1,4 +0,0 @@
-export interface KeyValueStore {
-  get<T = string>(key: string): Promise<T | null>;
-  set<T = string>(key: string, value: T, ttlSeconds?: number): Promise<void>;
-}
--- src/core/services/redis-service.ts
+++ src/core/services/redis-service.ts
@@ -1,34 +1,8 @@
 import { logger } from "../../config/logger";
 import { redis } from "../../config/redis-client";
-import { KeyValueStore } from "../contracts/storage/key-value-store";
 import { KeyValueListStore } from "../contracts/storage/key-value-list-store";
 
-export class RedisService implements KeyValueStore, KeyValueListStore {
-  async get<T = string>(key: string): Promise<T | null> {
-    try {
-      const value = await redis.get(key);
-      return value ? (JSON.parse(value) as T) : null;
-    } catch (err) {
-      logger.error(`Redis.get failed ${err}`, "[REDIS]");
-      return null;
-    }
-  }
-
-  async set<T = string>(
-    key: string,
-    value: T,
-    ttlSeconds?: number
-  ): Promise<void> {
-    try {
-      const data = JSON.stringify(value);
-      ttlSeconds
-        ? await redis.set(key, data, "EX", ttlSeconds)
-        : await redis.set(key, data);
-    } catch (err) {
-      logger.error(`Redis.set failed ${err}`, "[REDIS]");
-    }
-  }
-
+export class RedisService implements KeyValueListStore {
   async getList<T = string>(key: string): Promise<T[]> {
     try {
       const items = await redis.lrange(key, 0, -1);
