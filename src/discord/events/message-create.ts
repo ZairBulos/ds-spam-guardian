f4d92e3db30a420508a916d59347ff562b9c8433
Zair Bulos <buloszair@gmail.com>
2025-06-30 15:20:49 -0300

    feat: integrate spamDetector and cleaner in message-create event

--- src/discord/events/message-create.ts
+++ src/discord/events/message-create.ts
@@ -1,12 +1,7 @@
 import { Events, Message } from "discord.js";
 import { Event } from "../../core/interfaces/event";
-import { SpamCleaner } from "../../features/spam/services/spam-cleaner.service";
-import { SpamCleanupLock } from "../../features/spam/services/spam-cleanup-lock.service";
-import { spamDetector } from "../../features/spam/providers/spam-detector-memory.provider";
-import { logger } from "../../config/logger";
-
-const spamCleaner = new SpamCleaner();
-const spamCleanupLock = new SpamCleanupLock();
+import { redisSpamDetector } from "../../features/detector";
+import { clean } from "../../features/cleaner";
 
 class MessageCreateEvent implements Event<Events.MessageCreate> {
   public name = Events.MessageCreate;
@@ -14,16 +9,8 @@ class MessageCreateEvent implements Event<Events.MessageCreate> {
   public async execute(message: Message): Promise<void> {
     if (!message.guild) return;
 
-    const spamDetected = await spamDetector.isSpam(message);
-    if (!spamDetected) return;
-
-    logger.warn(
-      `Detected spam from ${message.author.username} in ${message.guild.name}: "${message.content}"`,
-      "[SPAM DETECT]"
-    );
-    void spamCleanupLock.run(message.author.id, message.guild.id, async () => {
-      await spamCleaner.deleteSpamMessages(message);
-    });
+    const isSpam = await redisSpamDetector.isSpam(message);
+    if (isSpam) clean(message);
   }
 }
 
