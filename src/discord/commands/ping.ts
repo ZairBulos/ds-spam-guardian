41cd60af600bbec340334e4aa393770791b7a108
Zair Bulos <buloszair@gmail.com>
2025-07-01 14:22:37 -0300

    feat: define base command interface and add initial ping command

---
+++ src/discord/commands/ping.ts
@@ -0,0 +1,14 @@
+import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Command } from "../../core/interfaces/command";
+
+class PingCommand implements Command {
+  data = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!");
+
+  public async execute(interaction: CommandInteraction): Promise<void> {
+    await interaction.reply("Pong!");
+  }
+}
+
+export default new PingCommand();
